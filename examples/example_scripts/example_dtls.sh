c925ba7c5ad558eb0ee881b711f3729483911de4
Eric Ackermann <eric.ackermann@student.hpi.de>
2022-07-07 12:06:16 +0200

    gdb failing server

--- examples/example_scripts/example_dtls.sh
+++ examples/example_scripts/example_dtls.sh
@@ -2,17 +2,19 @@
 # needs to be run from top-level directory, i.e. ./examples/example_scripts/example_kex.sh
 cd build/examples || exit 1
 
+echo -e "run r\nbt" > commands
+
 # let server generate certificates
-strace ../dtls_example_local r &
+gdb --command=commands ../dtls_example_local &
 SERVER_INITIAL_PID=$!
 
 sleep 5
 kill $SERVER_INITIAL_PID
 
-strace ../dtls_example_local s1 > client.log 2>&1 &
+../dtls_example_local s1 > client.log 2>&1 &
 CLIENT_PID=$!
 
-strace ../dtls_example_local r > server.log 2>&1
+../dtls_example_local r > server.log 2>&1
 SERVER_EXIT_CODE=$?
 
 # give client chance to terminate
